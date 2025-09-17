799dc336c5e29d3a3c8d5f44af0554aa84244869
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-09-17 20:47:47 +0200

    Php Variables

---
+++ Moduli 2/index.php
@@ -0,0 +1,32 @@
+<?php
+$Mbiemer = 'Qyqalla';
+echo "Gjin $Mbiemer <br>";
+
+$x=50;
+$y=20;
+echo $x+$y; echo"<br>";
+echo $x-$y; echo"<br>";
+echo $x%$y; echo"<br>";
+echo $x*$y; echo"<br>";
+echo $x/$y; echo"<br>";
+
+$a= 'Gjin';
+$b= 'Qyqalla';
+$c= $a.$b;
+
+echo"$c\n";
+
+$the_string= 'digital school';echo "<br>";
+echo strlen($the_string);echo "<br>";
+
+echo str_word_count($the_string);echo "<br>";
+
+$Programim= 'Programim is not cool';
+echo str_replace("not","very", $Programim);echo"<br>";
+
+$the_string='Qyqalla';
+echo strrev($the_string);echo"<br>";
+
+
+
+?>
