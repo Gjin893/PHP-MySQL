2efe476de66f2d0eb7acb1119630f8c0e5dfa7db
unknown <Student@DIGITALSCHOOL.LOCAL>
2025-10-01 11:29:25 -0700

    scope variables and arrays

---
+++ Moduli 4/arrays.php
@@ -0,0 +1,33 @@
+<?php
+/*
+$sports =['football','basketball','volleyball'];
+echo ($sports)[2];
+"<br>";
+echo end ($sports);
+"<br>";
+echo count ($sports);
+"<br>";
+
+$sports = array('football','basketball', 'volleyball');
+for ($i =0; $i <3; $i++){
+    echo $sports[$i],"\n";
+
+    array push
+}
+
+$sports = array('football','basketball', 'volleyball');
+ array_push($sports, 'Golf');
+ var_dump($sports);
+
+$sports = array('football','basketball', 'volleyball','golf');
+ array_unshift($sports,'tennis');
+ var_dump($sports);
+  
+ $sports = array('football','basketball', 'volleyball','golf');
+ array_unshift($sports,'tennis');
+ var_dump($sports);
+  */
+ $sports = array('football','basketball', 'volleyball','golf');
+ array_shift($sports,);
+ var_dump($sports);
+?>
