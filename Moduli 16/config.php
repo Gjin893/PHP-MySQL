03642332f9f339fcd1a0396c8fd92afac7b5fae7
unknown <Student@digitalschool.local>
2025-12-08 19:47:30 +0100

    Moduli 16

--- Moduli 16/config.php
+++ Moduli 16/config.php
@@ -1,19 +1,15 @@
 <?php
-
-
-
+//Initialization of some variables for maintaining database data
 $user="root";
 $pass="";
 $server="localhost";
 $dbname="mms";
 
-
 try {
-    
-    $conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
-
-
+	//Creating a PDO to connect with database
+	$conn =new PDO("mysql:host=$server;dbname=$dbname",$user,$pass);
 } catch (PDOException $e) {
-    echo "error: " . $e->getMessage();
+	echo "error: " . $e->getMessage();
 }
- ?>
+
+?>
